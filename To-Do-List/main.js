9caface9b47c511b3acd5d9ceb3072ffb7b4673a
majdeddine oumanni <majdeddineoumanni@gmail.com>
2024-05-02 19:03:03 +0100

    last version of all mini projects

--- To-Do-List/main.js
+++ To-Do-List/main.js
@@ -1,29 +1,30 @@
-let myInput = document.querySelector(".theTodoList");
-let myList = document.querySelector('.output');
-function outputList(){
-        // Create a new paragraph element
-        let listItem = document.createElement("p");
-
-        // Set the text content of the paragraph to the value of myInput
-        listItem.textContent = myInput.value;
-    
-        // Append the paragraph element to the output list
-        myList.appendChild(listItem);
-    
-        // Clear the input field
-        myInput.value = '';
-}
-/*
-    let myInput = document.querySelector('input');
-    let myList = document.querySelector('.output');
-    let myArray = [];
-    function appendList(){
-        let total = '';
-        myArray.push(myInput.value);
-        for(let i=0; i < myArray.length; i++){
-            total += myArray[i];
-        }
-        myList.innerHTML = `<p>${myArray.join(`<br>`)}</p>`;
-        myInput.value = '';
+let todoList =[];
+let listItems = document.querySelector('.list');
+function listAppear(){
+    let todoListHtml = '';
+    for(let i =0; i<todoList.length; i++){
+        let todoObject = todoList[i];
+        //let Name = todoObject.Name;
+        //let date = todoObject.date;
+        let { Name, date } = todoObject;
+        todoListHtml += `
+        <p>${Name}</p>
+        <p>${date}</p>
+        <button class="deleteButton" onclick="
+            todoList.splice(${i}, 1);
+            listAppear();
+        ">Delete</button>
+        `;
     }
-*/
+    listItems.innerHTML = todoListHtml;
+}
+function listIn(){
+    let listInput = document.querySelector('input');
+    let listDateElement = document.querySelector('.listDate');
+    todoList.push({
+        Name:listInput.value,
+        date: listDateElement.value
+    });
+    listInput.value = '';
+    listAppear();
+}
